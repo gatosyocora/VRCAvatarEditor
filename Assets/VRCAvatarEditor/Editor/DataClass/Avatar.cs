f7fce201bb8e328d862edf936bd59a857b106ca2
gatosyocora <kut190316q@gmail.com>
2019-10-28 22:52:03 +0900

    読み込み処理を追加

--- Assets/VRCAvatarEditor/Editor/DataClass/Avatar.cs
+++ Assets/VRCAvatarEditor/Editor/DataClass/Avatar.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using VRCSDK2;
 using System;
+using VRC.Core;
 
 namespace VRCAvatarEditor
 {
@@ -23,6 +24,10 @@ namespace VRCAvatarEditor
         public int triangleCountInactive { get; set; }
         public VRC_AvatarDescriptor.LipSyncStyle lipSyncStyle { get; set; }
         public Enum faceShapeKeyEnum { get; set; }
+        public List<SkinnedMesh> skinnedMeshList { get; set; }
+
+        public List<SkinnedMeshRenderer> skinnedMeshRendererList { get; set; }
+        public List<MeshRenderer> meshRendererList { get; set; }
 
         public Avatar()
         {
@@ -40,8 +45,49 @@ namespace VRCAvatarEditor
             triangleCountInactive = 0;
             lipSyncStyle = VRC_AvatarDescriptor.LipSyncStyle.Default;
             faceShapeKeyEnum = null;
+            skinnedMeshList = null;
         }
 
+        /// <summary>
+        /// アバターの情報を取得する
+        /// </summary>
+        public void LoadAvatarInfo()
+        {
+            if (descriptor == null) return;
+
+            var avatarObj = descriptor.gameObject;
+
+            animator = avatarObj.GetComponent<Animator>();
+
+            eyePos = descriptor.ViewPosition;
+            sex = descriptor.Animations;
+
+            standingAnimController = descriptor.CustomStandingAnims;
+            sittingAnimController = descriptor.CustomSittingAnims;
+
+            avatarId = descriptor.gameObject.GetComponent<PipelineManager>().blueprintId;
+
+            faceMesh = descriptor.VisemeSkinnedMesh;
+
+            if (faceMesh != null && descriptor.lipSync == VRC_AvatarDescriptor.LipSyncStyle.VisemeBlendShape)
+            {
+                lipSyncShapeKeyNames = new List<string>();
+                lipSyncShapeKeyNames.AddRange(descriptor.VisemeBlendShapes);
+            }
+
+            materials = GatoUtility.GetMaterials(avatarObj);
+
+            int triangleCountInactive = 0;
+            triangleCount = GatoUtility.GetAllTrianglesCount(avatarObj, ref triangleCountInactive);
+            this.triangleCountInactive = triangleCountInactive;
+
+            lipSyncStyle = descriptor.lipSync;
+
+            skinnedMeshList = FaceEmotion.GetSkinnedMeshListOfBlendShape(avatarObj);
+            
+            skinnedMeshRendererList = GatoUtility.GetSkinnedMeshList(avatarObj);
+            meshRendererList = GatoUtility.GetMeshList(avatarObj);
+        }
     }
 }
 
