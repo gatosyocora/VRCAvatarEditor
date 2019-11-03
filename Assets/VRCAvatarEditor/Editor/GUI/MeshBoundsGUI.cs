4f0b1e485609e65ee1d45a5a7d39120059585076
gatosyocora <kut190316q@gmail.com>
2019-11-03 23:51:31 +0900

    namespaceを追加

--- Assets/VRCAvatarEditor/Editor/GUI/MeshBoundsGUI.cs
+++ Assets/VRCAvatarEditor/Editor/GUI/MeshBoundsGUI.cs
@@ -5,129 +5,132 @@ using UnityEditor;
 using VRCAvatarEditor;
 using System.Linq;
 
-public class MeshBoundsGUI : Editor, IVRCAvatarEditorGUI
+namespace VRCAvatarEditor
 {
-    private VRCAvatarEditor.Avatar avatar;
-    
-    public List<SkinnedMeshRenderer> targetRenderers;
-    private List<SkinnedMeshRenderer> exclusions = new List<SkinnedMeshRenderer>();
-
-    public MeshBoundsGUI(ref VRCAvatarEditor.Avatar avatar)
+    public class MeshBoundsGUI : Editor, IVRCAvatarEditorGUI
     {
-        this.avatar = avatar;
-    }
+        private VRCAvatarEditor.Avatar avatar;
 
-    public bool DrawGUI(GUILayoutOption[] layoutOptions)
-    {
+        public List<SkinnedMeshRenderer> targetRenderers;
+        private List<SkinnedMeshRenderer> exclusions = new List<SkinnedMeshRenderer>();
 
-        // TODO: UIの見直し
-        if (targetRenderers == null && avatar != null)
+        public MeshBoundsGUI(ref VRCAvatarEditor.Avatar avatar)
         {
-            targetRenderers = MeshBounds.GetSkinnedMeshRenderersWithoutExclusions(
-                                avatar.descriptor.gameObject,
-                                exclusions);
+            this.avatar = avatar;
         }
 
-        EditorGUILayout.LabelField("Bounds", EditorStyles.boldLabel);
-
-        EditorGUILayout.Space();
-
-        using (new EditorGUILayout.HorizontalScope())
+        public bool DrawGUI(GUILayoutOption[] layoutOptions)
         {
-            GUILayout.FlexibleSpace();
 
-            if (GUILayout.Button("Reset Bounds to Prefab"))
+            // TODO: UIの見直し
+            if (targetRenderers == null && avatar != null)
             {
-                MeshBounds.RevertBoundsToPrefab(targetRenderers);
+                targetRenderers = MeshBounds.GetSkinnedMeshRenderersWithoutExclusions(
+                                    avatar.descriptor.gameObject,
+                                    exclusions);
             }
-        }
 
-        EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Bounds", EditorStyles.boldLabel);
 
-        using (new EditorGUILayout.VerticalScope(GUI.skin.box))
-        {
-            EditorGUILayout.LabelField("Exclusions");
+            EditorGUILayout.Space();
 
-            using (new EditorGUI.IndentLevelScope())
+            using (new EditorGUILayout.HorizontalScope())
             {
-                using (var check = new EditorGUI.ChangeCheckScope())
+                GUILayout.FlexibleSpace();
+
+                if (GUILayout.Button("Reset Bounds to Prefab"))
                 {
-                    var parentObject = EditorGUILayout.ObjectField(
-                        "Child objects",
-                        null,
-                        typeof(GameObject),
-                        true
-                    ) as GameObject;
-
-                    if (check.changed && parentObject != null && avatar != null)
-                    {
-                        var renderers = parentObject.GetComponentsInChildren<SkinnedMeshRenderer>();
-                        foreach (var renderer in renderers)
-                        {
-                            exclusions.Add(renderer);
-                        }
-                        exclusions = exclusions.Distinct().ToList();
-
-                        targetRenderers = MeshBounds.GetSkinnedMeshRenderersWithoutExclusions(
-                                                        avatar.descriptor.gameObject,
-                                                        exclusions);
-                    }
+                    MeshBounds.RevertBoundsToPrefab(targetRenderers);
                 }
+            }
 
-                EditorGUILayout.Space();
+            EditorGUILayout.Space();
 
-                using (new EditorGUILayout.HorizontalScope())
+            using (new EditorGUILayout.VerticalScope(GUI.skin.box))
+            {
+                EditorGUILayout.LabelField("Exclusions");
+
+                using (new EditorGUI.IndentLevelScope())
                 {
-                    GUILayout.FlexibleSpace();
-
-                    if (GUILayout.Button("+", GUILayout.MaxWidth(60)))
+                    using (var check = new EditorGUI.ChangeCheckScope())
                     {
-                        exclusions.Add(null);
-                    }
-                }
+                        var parentObject = EditorGUILayout.ObjectField(
+                            "Child objects",
+                            null,
+                            typeof(GameObject),
+                            true
+                        ) as GameObject;
 
-                using (var check = new EditorGUI.ChangeCheckScope())
-                {
-
-                    for (int i = 0; i < exclusions.Count; i++)
-                    {
-                        using (new EditorGUILayout.HorizontalScope())
+                        if (check.changed && parentObject != null && avatar != null)
                         {
-                            exclusions[i] = EditorGUILayout.ObjectField(
-                                "Object " + (i + 1),
-                                exclusions[i],
-                                typeof(SkinnedMeshRenderer),
-                                true
-                            ) as SkinnedMeshRenderer;
-
-                            if (GUILayout.Button("x", GUILayout.MaxWidth(30)))
+                            var renderers = parentObject.GetComponentsInChildren<SkinnedMeshRenderer>();
+                            foreach (var renderer in renderers)
                             {
-                                exclusions.RemoveAt(i);
+                                exclusions.Add(renderer);
+                            }
+                            exclusions = exclusions.Distinct().ToList();
+
+                            targetRenderers = MeshBounds.GetSkinnedMeshRenderersWithoutExclusions(
+                                                            avatar.descriptor.gameObject,
+                                                            exclusions);
+                        }
+                    }
+
+                    EditorGUILayout.Space();
+
+                    using (new EditorGUILayout.HorizontalScope())
+                    {
+                        GUILayout.FlexibleSpace();
+
+                        if (GUILayout.Button("+", GUILayout.MaxWidth(60)))
+                        {
+                            exclusions.Add(null);
+                        }
+                    }
+
+                    using (var check = new EditorGUI.ChangeCheckScope())
+                    {
+
+                        for (int i = 0; i < exclusions.Count; i++)
+                        {
+                            using (new EditorGUILayout.HorizontalScope())
+                            {
+                                exclusions[i] = EditorGUILayout.ObjectField(
+                                    "Object " + (i + 1),
+                                    exclusions[i],
+                                    typeof(SkinnedMeshRenderer),
+                                    true
+                                ) as SkinnedMeshRenderer;
+
+                                if (GUILayout.Button("x", GUILayout.MaxWidth(30)))
+                                {
+                                    exclusions.RemoveAt(i);
+                                }
                             }
                         }
+
+                        if (check.changed && avatar != null)
+                        {
+                            targetRenderers = MeshBounds.GetSkinnedMeshRenderersWithoutExclusions(
+                                                avatar.descriptor.gameObject,
+                                                exclusions);
+                        }
                     }
 
-                    if (check.changed && avatar != null)
-                    {
-                        targetRenderers = MeshBounds.GetSkinnedMeshRenderersWithoutExclusions(
-                                            avatar.descriptor.gameObject,
-                                            exclusions);
-                    }
+                    EditorGUILayout.Space();
                 }
-
-                EditorGUILayout.Space();
             }
+
+            if (GUILayout.Button("Set Bounds"))
+            {
+                MeshBounds.BoundsSetter(targetRenderers);
+            }
+            return false;
         }
 
-        if (GUILayout.Button("Set Bounds"))
-        {
-            MeshBounds.BoundsSetter(targetRenderers);
-        }
-        return false;
+        public void DrawSettingsGUI() { }
+        public void LoadSettingData(SettingData settingAsset) { }
+        public void SaveSettingData(ref SettingData settingAsset) { }
+        public void Dispose() { }
     }
-
-    public void DrawSettingsGUI() { }
-    public void LoadSettingData(SettingData settingAsset) { }
-    public void SaveSettingData(ref SettingData settingAsset) { }
-    public void Dispose() { }
 }
--- Assets/VRCAvatarEditor/Editor/GUI/ProbeAnchorGUI.cs
+++ Assets/VRCAvatarEditor/Editor/GUI/ProbeAnchorGUI.cs
@@ -4,123 +4,127 @@ using UnityEngine;
 using UnityEditor;
 using VRCAvatarEditor;
 
-public class ProbeAnchorGUI : Editor, IVRCAvatarEditorGUI
+namespace VRCAvatarEditor
 {
-    private VRCAvatarEditor.Avatar avatar;
-
-    private ProbeAnchor.TARGETPOS targetPos = ProbeAnchor.TARGETPOS.HEAD;
-
-    private bool isGettingSkinnedMeshRenderer = true;
-    private bool isGettingMeshRenderer = true;
-
-    private bool isOpeningRendererList = false;
-
-    private bool[] isSettingToSkinnedMesh = null;
-    private bool[] isSettingToMesh = null;
-
-    private Vector2 leftScrollPos = Vector2.zero;
-
-    public ProbeAnchorGUI(ref VRCAvatarEditor.Avatar avatar)
+    public class ProbeAnchorGUI : Editor, IVRCAvatarEditorGUI
     {
-        this.avatar = avatar;
-    }
+        private VRCAvatarEditor.Avatar avatar;
 
-    public bool DrawGUI(GUILayoutOption[] layoutOptions) {
-        EditorGUILayout.LabelField("Probe Anchor", EditorStyles.boldLabel);
+        private ProbeAnchor.TARGETPOS targetPos = ProbeAnchor.TARGETPOS.HEAD;
 
-        using (new EditorGUILayout.VerticalScope(GUI.skin.box))
+        private bool isGettingSkinnedMeshRenderer = true;
+        private bool isGettingMeshRenderer = true;
+
+        private bool isOpeningRendererList = false;
+
+        private bool[] isSettingToSkinnedMesh = null;
+        private bool[] isSettingToMesh = null;
+
+        private Vector2 leftScrollPos = Vector2.zero;
+
+        public ProbeAnchorGUI(ref VRCAvatarEditor.Avatar avatar)
         {
-            // 設定するRendererの選択
-            isGettingSkinnedMeshRenderer = EditorGUILayout.Toggle("Set To SkinnedMeshRenderer", isGettingSkinnedMeshRenderer);
-            isGettingMeshRenderer = EditorGUILayout.Toggle("Set To MeshRenderer", isGettingMeshRenderer);
+            this.avatar = avatar;
+        }
 
-            // ライティングの計算の基準とする位置を選択
-            targetPos = (ProbeAnchor.TARGETPOS)EditorGUILayout.EnumPopup("TargetPosition", targetPos);
+        public bool DrawGUI(GUILayoutOption[] layoutOptions)
+        {
+            EditorGUILayout.LabelField("Probe Anchor", EditorStyles.boldLabel);
 
-            // Rendererの一覧を表示
-            if (avatar.descriptor != null)
+            using (new EditorGUILayout.VerticalScope(GUI.skin.box))
             {
-                isOpeningRendererList = EditorGUILayout.Foldout(isOpeningRendererList, "Renderer List");
+                // 設定するRendererの選択
+                isGettingSkinnedMeshRenderer = EditorGUILayout.Toggle("Set To SkinnedMeshRenderer", isGettingSkinnedMeshRenderer);
+                isGettingMeshRenderer = EditorGUILayout.Toggle("Set To MeshRenderer", isGettingMeshRenderer);
 
-                if (isOpeningRendererList)
+                // ライティングの計算の基準とする位置を選択
+                targetPos = (ProbeAnchor.TARGETPOS)EditorGUILayout.EnumPopup("TargetPosition", targetPos);
+
+                // Rendererの一覧を表示
+                if (avatar.descriptor != null)
                 {
-                    using (var scrollView = new EditorGUILayout.ScrollViewScope(leftScrollPos))
+                    isOpeningRendererList = EditorGUILayout.Foldout(isOpeningRendererList, "Renderer List");
+
+                    if (isOpeningRendererList)
                     {
-                        leftScrollPos = scrollView.scrollPosition;
-
-                        using (new EditorGUI.IndentLevelScope())
+                        using (var scrollView = new EditorGUILayout.ScrollViewScope(leftScrollPos))
                         {
-                            int index = 0;
+                            leftScrollPos = scrollView.scrollPosition;
 
-                            if (isGettingSkinnedMeshRenderer && avatar.skinnedMeshRendererList != null && isSettingToSkinnedMesh != null)
+                            using (new EditorGUI.IndentLevelScope())
                             {
-                                foreach (var skinnedMesh in avatar.skinnedMeshRendererList)
+                                int index = 0;
+
+                                if (isGettingSkinnedMeshRenderer && avatar.skinnedMeshRendererList != null && isSettingToSkinnedMesh != null)
                                 {
-                                    if (skinnedMesh == null) continue;
-
-                                    using (new GUILayout.HorizontalScope())
+                                    foreach (var skinnedMesh in avatar.skinnedMeshRendererList)
                                     {
-                                        isSettingToSkinnedMesh[index] = EditorGUILayout.Toggle(skinnedMesh.gameObject.name, isSettingToSkinnedMesh[index]);
-                                        if (GUILayout.Button("Select"))
-                                            Selection.activeGameObject = skinnedMesh.gameObject;
-                                    }
+                                        if (skinnedMesh == null) continue;
 
-                                    index++;
+                                        using (new GUILayout.HorizontalScope())
+                                        {
+                                            isSettingToSkinnedMesh[index] = EditorGUILayout.Toggle(skinnedMesh.gameObject.name, isSettingToSkinnedMesh[index]);
+                                            if (GUILayout.Button("Select"))
+                                                Selection.activeGameObject = skinnedMesh.gameObject;
+                                        }
+
+                                        index++;
+                                    }
+                                }
+
+                                index = 0;
+
+                                if (isGettingMeshRenderer && avatar.meshRendererList != null && isSettingToMesh != null)
+                                {
+                                    foreach (var mesh in avatar.meshRendererList)
+                                    {
+                                        if (mesh == null) continue;
+
+                                        using (new GUILayout.HorizontalScope())
+                                        {
+                                            isSettingToMesh[index] = EditorGUILayout.Toggle(mesh.gameObject.name, isSettingToMesh[index]);
+                                            if (GUILayout.Button("Select"))
+                                                Selection.activeGameObject = mesh.gameObject;
+                                        }
+
+                                        index++;
+
+                                    }
                                 }
                             }
 
-                            index = 0;
-
-                            if (isGettingMeshRenderer && avatar.meshRendererList != null && isSettingToMesh != null)
-                            {
-                                foreach (var mesh in avatar.meshRendererList)
-                                {
-                                    if (mesh == null) continue;
-
-                                    using (new GUILayout.HorizontalScope())
-                                    {
-                                        isSettingToMesh[index] = EditorGUILayout.Toggle(mesh.gameObject.name, isSettingToMesh[index]);
-                                        if (GUILayout.Button("Select"))
-                                            Selection.activeGameObject = mesh.gameObject;
-                                    }
-
-                                    index++;
-
-                                }
-                            }
+                            EditorGUILayout.HelpBox("チェックがついているメッシュのProbeAnchorが設定されます", MessageType.Info);
                         }
-
-                        EditorGUILayout.HelpBox("チェックがついているメッシュのProbeAnchorが設定されます", MessageType.Info);
                     }
                 }
             }
-        }
 
-        if (GUILayout.Button("Set ProbeAnchor"))
+            if (GUILayout.Button("Set ProbeAnchor"))
+            {
+                GameObject anchorTarget = null;
+                var result = ProbeAnchor.CreateAndSetProbeAnchorObject(avatar.descriptor.gameObject, targetPos, ref anchorTarget);
+                if (result && isGettingSkinnedMeshRenderer)
+                    ProbeAnchor.SetProbeAnchorToSkinnedMeshRenderers(ref anchorTarget, ref avatar, ref isSettingToSkinnedMesh);
+                if (result && isGettingMeshRenderer)
+                    ProbeAnchor.SetProbeAnchorToMeshRenderers(ref anchorTarget, ref avatar, ref isSettingToMesh);
+            }
+
+            return false;
+        }
+        public void DrawSettingsGUI() { }
+        public void LoadSettingData(SettingData settingAsset) { }
+        public void SaveSettingData(ref SettingData settingAsset) { }
+        public void Dispose() { }
+
+        public void SettingForProbeSetter()
         {
-            GameObject anchorTarget = null;
-            var result = ProbeAnchor.CreateAndSetProbeAnchorObject(avatar.descriptor.gameObject, targetPos, ref anchorTarget);
-            if (result && isGettingSkinnedMeshRenderer)
-                ProbeAnchor.SetProbeAnchorToSkinnedMeshRenderers(ref anchorTarget, ref avatar, ref isSettingToSkinnedMesh);
-            if (result && isGettingMeshRenderer)
-                ProbeAnchor.SetProbeAnchorToMeshRenderers(ref anchorTarget, ref avatar, ref isSettingToMesh);
+            if (avatar.skinnedMeshRendererList == null || avatar.meshRendererList == null)
+                return;
+
+            isSettingToSkinnedMesh = new bool[avatar.skinnedMeshRendererList.Count];
+            for (int i = 0; i < avatar.skinnedMeshRendererList.Count; i++) isSettingToSkinnedMesh[i] = true;
+            isSettingToMesh = new bool[avatar.meshRendererList.Count];
+            for (int i = 0; i < avatar.meshRendererList.Count; i++) isSettingToMesh[i] = true;
         }
-
-        return false;
-    }
-    public void DrawSettingsGUI() { }
-    public void LoadSettingData(SettingData settingAsset) { }
-    public void SaveSettingData(ref SettingData settingAsset) { }
-    public void Dispose() { }
-
-    public void SettingForProbeSetter()
-    {
-        if (avatar.skinnedMeshRendererList == null || avatar.meshRendererList == null)
-            return;
-
-        isSettingToSkinnedMesh = new bool[avatar.skinnedMeshRendererList.Count];
-        for (int i = 0; i < avatar.skinnedMeshRendererList.Count; i++) isSettingToSkinnedMesh[i] = true;
-        isSettingToMesh = new bool[avatar.meshRendererList.Count];
-        for (int i = 0; i < avatar.meshRendererList.Count; i++) isSettingToMesh[i] = true;
     }
 }
