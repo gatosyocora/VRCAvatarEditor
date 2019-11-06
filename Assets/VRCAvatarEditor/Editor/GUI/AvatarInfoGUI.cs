7e0649e2ed3162891c7a9f1c78995b1a3baa54b2
gatosyocora <kut190316q@gmail.com>
2019-11-07 00:37:33 +0900

    ViewPositionを自動で計算する機能を追加（暫定）

--- Assets/VRCAvatarEditor/Editor/GUI/AvatarInfoGUI.cs
+++ Assets/VRCAvatarEditor/Editor/GUI/AvatarInfoGUI.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using VRCSDK2;
 using UnityEditor;
 using System;
+using System.Linq;
 
 namespace VRCAvatarEditor
 {
@@ -64,7 +65,28 @@ namespace VRCAvatarEditor
                     }
                 }
 
+                // ポリゴン数
                 EditorGUILayout.LabelField("Triangles", avatar.triangleCount + "(" + (avatar.triangleCount + avatar.triangleCountInactive) + ")");
+
+                // View Position
+                using (new EditorGUILayout.HorizontalScope())
+                {
+                    using (var check = new EditorGUI.ChangeCheckScope())
+                    {
+                        avatar.eyePos = EditorGUILayout.Vector3Field("View Position", avatar.eyePos);
+
+                        if (check.changed)
+                        {
+                            avatar.descriptor.ViewPosition = avatar.eyePos;
+                        }
+
+                        if (GUILayout.Button("Auto Setting"))
+                        {
+                            avatar.eyePos = CalcAvatarViewPosition(avatar);
+                            avatar.descriptor.ViewPosition = avatar.eyePos;
+                        }
+                    }
+                }
 
                 // リップシンク
                 string lipSyncWarningMessage = "リップシンクが正しく設定されていない可能性があります";
@@ -124,6 +146,56 @@ namespace VRCAvatarEditor
         public void LoadSettingData(SettingData settingAsset) { }
         public void SaveSettingData(ref SettingData settingAsset) { }
         public void Dispose() { }
+
+        private Vector3 CalcAvatarViewPosition(VRCAvatarEditor.Avatar avatar)
+        {
+            var viewPos = Vector3.zero;
+            var animator = avatar.animator;
+            var objTrans = avatar.descriptor.transform;
+
+            // leftEyeとRightEyeの位置からx, yを計算する
+            var leftEyeTrans = animator.GetBoneTransform(HumanBodyBones.LeftEye);
+            var rightEyeTrans = animator.GetBoneTransform(HumanBodyBones.RightEye);
+            viewPos = (leftEyeTrans.position + rightEyeTrans.position) / 2f;
+
+
+            var renderer = avatar.faceMesh;
+            var mesh = renderer.sharedMesh;
+            var vertices = mesh.vertices;
+            var local2WorldMat = renderer.transform.localToWorldMatrix;
+
+            // 左目メッシュの頂点のうち, 左目ボーンから一番離れた頂点までの距離を計算する
+            var leftEyeBoneIndex = Array.IndexOf(renderer.bones, leftEyeTrans);
+            var leftEyeMeshVertexIndices = mesh.boneWeights
+                                .Select((x, index) => new { index = index, boneIndex0 = x.boneIndex0, boneIndex1 = x.boneIndex1, boneIndex2 = x.boneIndex2, boneIndex3 = x.boneIndex3})
+                                .Where(x => x.boneIndex0 == leftEyeBoneIndex || x.boneIndex1 == leftEyeBoneIndex || x.boneIndex2 == leftEyeBoneIndex || x.boneIndex3 == leftEyeBoneIndex)
+                                .Select(x => x.index)
+                                .ToArray();
+            var leftMaxDistance = 0f;
+            foreach (var index in leftEyeMeshVertexIndices)
+                if (leftMaxDistance < Vector3.Distance(local2WorldMat.MultiplyPoint3x4(vertices[index]), leftEyeTrans.position))
+                    leftMaxDistance = Vector3.Distance(local2WorldMat.MultiplyPoint3x4(vertices[index]), leftEyeTrans.position);
+
+
+            // 右目も同じく計算する
+            var rightEyeBoneIndex = Array.IndexOf(renderer.bones, rightEyeTrans);
+            var rightEyeMeshVertexIndices = mesh.boneWeights
+                                .Select((x, index) => new { index = index, boneIndex0 = x.boneIndex0, boneIndex1 = x.boneIndex1, boneIndex2 = x.boneIndex2, boneIndex3 = x.boneIndex3 })
+                                .Where(x => x.boneIndex0 == rightEyeBoneIndex || x.boneIndex1 == rightEyeBoneIndex || x.boneIndex2 == rightEyeBoneIndex || x.boneIndex3 == rightEyeBoneIndex)
+                                .Select(x => x.index)
+                                .ToArray();
+            var rightMaxDistance = 0f;
+            foreach (var index in rightEyeMeshVertexIndices)
+                if (rightMaxDistance < Vector3.Distance(local2WorldMat.MultiplyPoint3x4(vertices[index]), rightEyeTrans.position))
+                    rightMaxDistance = Vector3.Distance(local2WorldMat.MultiplyPoint3x4(vertices[index]), rightEyeTrans.position);
+
+            viewPos.z += (leftMaxDistance + rightMaxDistance) / 2f;
+
+            // ローカル座標に変換
+            viewPos = objTrans.worldToLocalMatrix.MultiplyPoint3x4(viewPos);
+
+            return viewPos;
+        }
     }
 }
 
