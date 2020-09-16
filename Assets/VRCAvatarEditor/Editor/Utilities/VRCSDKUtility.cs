b1c3f17b3100d6f9edf10e30e3523252cf8cd182
gatosyocora <gatosyocora36@gmail.com>
2020-09-16 16:22:44 +0900

    VRCSDKに含まれているアセットの一部が別フォルダに移動したことで見つからなくなっていた不具合を修正

--- Assets/VRCAvatarEditor/Editor/Utilities/VRCSDKUtility.cs
+++ Assets/VRCAvatarEditor/Editor/Utilities/VRCSDKUtility.cs
@@ -29,7 +29,7 @@ namespace VRCAvatarEditor.Utilities
             // VRCSDKフォルダが移動されている可能性があるため対象ファイルを探す
             return AssetDatabase.FindAssets(fileName)
                         .Select(g => AssetDatabase.GUIDToAssetPath(g))
-                        .Where(p => p.Contains("/VRCSDK/"))
+                        .Where(p => p.Contains("/VRCSDK/") || p.Contains("VRChat Examples"))
                         .OrderBy(p => Path.GetFileName(p).Count())
                         .FirstOrDefault();
         }
