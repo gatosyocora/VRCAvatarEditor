b0826b33ad7642658b065ebe7889657a21c2c905
gatosyocora <gatosyocora36@gmail.com>
2020-11-22 19:48:20 +0900

    アバター選択時にエラーが出ないことを検証するテストを作成

---
+++ Assets/VRCAvatarEditorTest/Editor/Basic/SelectAvatar.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using VRCSDK2;
+
+namespace VRCAvatarEditor.Test.Basic
+{
+    public class SelectAvatar
+    {
+        [Test]
+        public void HaveNoError()
+        {
+            var testAvatars = Resources.Load<TestAvatars>("TestAvatars");
+            var newScene = EditorSceneManager.NewScene(NewSceneSetup.DefaultGameObjects, NewSceneMode.Single);
+            var editorGUI = ScriptableObject.CreateInstance<VRCAvatarEditorGUI>();
+            
+            foreach (Object avatarPrefab in testAvatars.testAvatars)
+            {
+                Debug.Log($"[{avatarPrefab.name}]");
+                var avatar = PrefabUtility.InstantiatePrefab(avatarPrefab) as GameObject;
+                editorGUI.TargetAvatarDescriptor = avatar.GetComponent<VRC_AvatarDescriptor>();
+            }
+        }
+    }
+}
