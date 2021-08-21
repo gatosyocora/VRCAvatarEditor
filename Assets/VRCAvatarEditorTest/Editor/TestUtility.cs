c30fdaf457cb64970ca9a6ec5ae1fce6694a370f
gatosyocora <gatosyocora36@gmail.com>
2021-08-21 13:33:04 +0900

    テストアバターを取得するメソッドを追加

---
+++ Assets/VRCAvatarEditorTest/Editor/TestUtility.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace VRCAvatarEditor.Test
+{
+    public static class TestUtility
+    {
+        public static TestAvatars GetTestAvatars()
+        {
+            return AssetDatabase.LoadAssetAtPath<TestAvatars>(
+                "Assets/VRCAvatarEditorTest/Editor/TestAvatars.asset"
+            );
+        }
+    }
+}
