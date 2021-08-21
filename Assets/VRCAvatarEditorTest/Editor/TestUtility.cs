50eeb56fdc32ef4c25c545460fd64e8c6e80d138
gatosyocora <gatosyocora36@gmail.com>
2021-08-21 14:30:11 +0900

    TestSceneとそれを読み込むメソッドを作成

--- Assets/VRCAvatarEditorTest/Editor/TestUtility.cs
+++ Assets/VRCAvatarEditorTest/Editor/TestUtility.cs
@@ -1,12 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace VRCAvatarEditor.Test
 {
     public static class TestUtility
     {
+        public static Scene OpenTestScene()
+        {
+            return EditorSceneManager.OpenScene(
+                "Assets/VRCAvatarEditorTest/Editor/TestScene.unity",
+                OpenSceneMode.Single
+            );
+        }
+
         public static TestAvatars GetTestAvatars()
         {
             return AssetDatabase.LoadAssetAtPath<TestAvatars>(
