ca626cd8c8034e8c44a989daac25bfd6ec91fe62
gatosyocora <gatosyocora36@gmail.com>
2021-08-21 15:31:59 +0900

    FXレイヤーとGestureレイヤーを取得するテストを追加

---
+++ Assets/VRCAvatarEditorTest/Editor/Utilities/VRCAvatarAnimationUtilityTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Animations;
+using UnityEngine;
+using VRC.SDK3.Avatars.Components;
+using VRCAvatarEditor.Utilities;
+
+namespace VRCAvatarEditor.Test
+{
+    public class VRCAvatarAnimationUtilityTest
+    {
+        static List<Object> avatarPrefabs = TestUtility.GetTestAvatars().avatarPrefabs.ToList();
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            TestUtility.OpenTestScene();
+        }
+
+        [TestCaseSource("avatarPrefabs")]
+        public void GetPlayableLayerでFXLayerが取得できる(GameObject avatarPrefab)
+        {
+            var avatarObject = PrefabUtility.InstantiatePrefab(avatarPrefab) as GameObject;
+            var descripter = avatarObject.GetComponent<VRCAvatarDescriptor>();
+            var fxLayer = VRCAvatarAnimationUtility.GetPlayableLayer(descripter, VRCAvatarDescriptor.AnimLayerType.FX);
+            Assert.IsNotNull(fxLayer);
+        }
+
+        [TestCaseSource("avatarPrefabs")]
+        public void GetPlayableLayerでGestureLayerが取得できる(GameObject avatarPrefab)
+        {
+            var avatarObject = PrefabUtility.InstantiatePrefab(avatarPrefab) as GameObject;
+            var descripter = avatarObject.GetComponent<VRCAvatarDescriptor>();
+            var gestureLayer = VRCAvatarAnimationUtility.GetPlayableLayer(descripter, VRCAvatarDescriptor.AnimLayerType.Gesture);
+            Assert.IsNotNull(gestureLayer);
+        }
+    }
+}
