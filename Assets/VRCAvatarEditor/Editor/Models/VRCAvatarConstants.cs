bf760e92188affaf0a01e211c5b8727714b6e1a1
gatosyocora <gatosyocora36@gmail.com>
2021-08-21 12:00:13 +0900

    リセット用レイヤーの名前を変更

--- Assets/VRCAvatarEditor/Editor/Models/VRCAvatarConstants.cs
+++ Assets/VRCAvatarEditor/Editor/Models/VRCAvatarConstants.cs
@@ -10,7 +10,7 @@ namespace VRCAvatarEditor
         public const string FX_RIGHT_HAND_LAYER_NAME = "Right Hand";
         public const string IDLE_STATE_NAME = "Idle";
         public const string EMPTY_ANIMATION_NAME = "Empty";
-        public const string FX_DEFAULT_LAYER_NAME = "DefaultFace";
+        public const string FX_DEFAULT_LAYER_NAME = "Reset";
         public const string DEFAULT_FACE_STATE_NAME = "Reset";
         public const string DEFAULT_FACE_ANIMATION_NAME = "DefaultFace";
 
