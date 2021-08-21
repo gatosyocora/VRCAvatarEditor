126b581ef837990c80dd6cc1ee609e62c4260ec7
gatosyocora <gatosyocora36@gmail.com>
2021-08-21 13:27:38 +0900

    Testアバターを管理する用のScriptableObjectを追加

---
+++ Assets/VRCAvatarEditorTest/Editor/TestAvatars.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace VRCAvatarEditor.Test
+{
+    [CreateAssetMenu(menuName = "TestAvatars")]
+    public class TestAvatars : ScriptableObject
+    {
+        public Object[] avatarPrefabs;
+    }
+}
