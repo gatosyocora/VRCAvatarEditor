eb7885112492cbaf0a9c32ba032ad3e7b12390c0
gatosyocora <gatosyocora36@gmail.com>
2021-08-22 17:33:24 +0900

    テスト用に一時的にアセットファイルを作成するためのメソッドを追加

--- Assets/VRCAvatarEditorTest/Editor/TestUtility.cs
+++ Assets/VRCAvatarEditorTest/Editor/TestUtility.cs
@@ -9,6 +9,8 @@ namespace VRCAvatarEditor.Test
     public static class TestUtility
     {
         private const string TEST_FOLDER_PATH = "Assets/VRCAvatarEditorTest/Editor";
+        private const string CACHE_FOLDER_NAME = "Cache";
+        public static readonly string CACHE_FOLDER_PATH = Path.Combine(TEST_FOLDER_PATH, CACHE_FOLDER_NAME);
         private static readonly string TEST_SCENE_PATH = Path.Combine(TEST_FOLDER_PATH, "TestScene.unity");
         private static readonly string TEST_AVATARS_ASSET_PATH = Path.Combine(TEST_FOLDER_PATH, "TestAvatars.asset");
 
@@ -32,6 +34,37 @@ namespace VRCAvatarEditor.Test
             if (behaviour == null) return null;
             Object.DestroyImmediate(behaviour);
             return gameObject.AddComponent<T>();
+        }
+
+        public static string CreateCacheFolder()
+        {
+            if (AssetDatabase.IsValidFolder(CACHE_FOLDER_PATH))
+            {
+                return CACHE_FOLDER_PATH;
+            }
+
+            AssetDatabase.CreateFolder(TEST_FOLDER_PATH, CACHE_FOLDER_NAME);
+            return CACHE_FOLDER_PATH;
+        }
+
+        public static void DeleteCache()
+        {
+            AssetDatabase.DeleteAsset(CACHE_FOLDER_PATH);
+        }
+
+        public static T DuplicateAssetToCache<T>(Object asset) where T : Object
+        {
+            if (!AssetDatabase.IsValidFolder(CACHE_FOLDER_PATH))
+            {
+                CreateCacheFolder();
+            }
+
+            var assetPath = AssetDatabase.GetAssetPath(asset);
+            var newPath = AssetDatabase.GenerateUniqueAssetPath(
+                Path.Combine(CACHE_FOLDER_PATH, Path.GetFileName(assetPath))
+            );
+            AssetDatabase.CopyAsset(assetPath, newPath);
+            return AssetDatabase.LoadAssetAtPath<T>(newPath);
         }
     }
 }
