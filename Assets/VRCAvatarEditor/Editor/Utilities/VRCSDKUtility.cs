b58551e315e01782ebc3d0a0a9a7fa62b6bb9eb6
gatosyocora <gatosyocora36@gmail.com>
2022-09-03 17:33:07 +0900

    使用しなくなったので削除

--- Assets/VRCAvatarEditor/Editor/Utilities/VRCSDKUtility.cs
+++ Assets/VRCAvatarEditor/Editor/Utilities/VRCSDKUtility.cs
@@ -1,24 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using UnityEditor;
-using UnityEngine;
 
 namespace VRCAvatarEditor.Utilities
 {
     public class VRCSDKUtility
     {
-        /// <summary>
-        /// VRCSDKのバージョンを取得する
-        /// </summary>
-        /// <returns></returns>
-        public static string GetVRCSDKVersion()
-        {
-            string path = GetVRCSDKFilePath("version");
-            return FileUtility.GetFileTexts(path);
-        }
-
         /// <summary>
         /// VRCSDKに含まれるファイルを取得する
         /// </summary>
@@ -32,28 +19,6 @@ namespace VRCAvatarEditor.Utilities
                         .Where(p => p.Contains("/VRCSDK/") || p.Contains("VRChat Examples"))
                         .OrderBy(p => Path.GetFileName(p).Count())
                         .FirstOrDefault();
-        }
-
-        /// <summary>
-        /// VRCSDKが新しいUIかどうか
-        /// </summary>
-        /// <returns></returns>
-        public static bool IsNewSDKUI()
-        {
-            var sdkVersion = GetVRCSDKVersion();
-            // 新UI以降のバージョンにはファイルが存在するため何かしらは返ってくる
-            if (string.IsNullOrEmpty(sdkVersion)) return false;
-
-            var dotChar = '.';
-            var zero = '0';
-            var versions = sdkVersion.Split(dotChar);
-            var version =
-                    versions[0].PadLeft(4, zero) + dotChar +
-                    versions[1].PadLeft(2, zero) + dotChar +
-                    versions[2].PadLeft(2, zero);
-            var newVersion = "2019.08.23";
-
-            return newVersion.CompareTo(version) <= 0;
         }
 
         public static void UploadAvatar(bool newSDKUI)
